8d37f38ee0fc652091fcf1bd6c6e28f2b06abe61
1_am_a_geek <tmy0x3@icloud.com>
2016-08-15 14:31:17 +0900

    Initial Commit

---
+++ Salada/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Salada
+//
+//  Created by 1amageek on 2016/08/15.
+//  Copyright © 2016年 Stamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
