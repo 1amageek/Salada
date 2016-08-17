ee7fa6dc20ca41d5f7b480b3c467e1007c985071
1_am_a_geek <tmy0x3@icloud.com>
2016-08-17 18:24:13 +0900

    Add Salada lib

---
+++ Salada/Salada.h
@@ -0,0 +1,19 @@
+//
+//  Salada.h
+//  Salada
+//
+//  Created by 1amageek on 2016/08/17.
+//  Copyright © 2016年 Stamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Salada.
+FOUNDATION_EXPORT double SaladaVersionNumber;
+
+//! Project version string for Salada.
+FOUNDATION_EXPORT const unsigned char SaladaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Salada/PublicHeader.h>
+
+
