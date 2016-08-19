6a7b3dafbc72a9f8f8b6a66c98bd0081506b5333
1_am_a_geek <tmy0x3@icloud.com>
2016-08-19 12:26:42 +0900

    Add Store

---
+++ Store/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Salada
+//
+//  Created by 1amageek on 2016/08/19.
+//  Copyright © 2016年 Stamp. All rights reserved.
+//
+
+import Foundation
+
+class Item: Ingredient {
+    
+    var title: String?
+    var price: 100
+    var url: String?
+    
+}
