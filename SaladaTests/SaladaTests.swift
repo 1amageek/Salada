870ddbd01bb8d37cb89ea0550a0ef51382ab3dce
1_am_a_geek <tmy0x3@icloud.com>
2016-10-18 13:49:13 +0900

    Add test

---
+++ SaladaTests/SaladaTests.swift
@@ -0,0 +1,44 @@
+//
+//  SaladaTests.swift
+//  SaladaTests
+//
+//  Created by 1amageek on 2016/10/18.
+//  Copyright © 2016年 Stamp. All rights reserved.
+//
+
+import XCTest
+import Firebase
+import FirebaseDatabase
+import FirebaseStorage
+
+class SaladaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    func testObjectValues() {
+        FIRApp.configure()
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
