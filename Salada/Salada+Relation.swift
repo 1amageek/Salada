06b24a3a4465ec8eac0d21f17a8e4ededc4822f5
1_am_a_geek <tmy0x3@icloud.com>
2017-02-09 12:10:33 +0900

    Bugfix relation

--- Salada/Salada+Relation.swift
+++ Salada/Salada+Relation.swift
@@ -74,19 +74,28 @@ extension Salada {
         public func index(_ i: Int, offsetBy n: Int, limitedBy limit: Int) -> Int? {
             return _Self.index(i, offsetBy: n, limitedBy: limit)
         }
-
+        
+        public func objects(at indexes: IndexSet) -> [Element] {
+            return indexes.filter { $0 < self.count }.map{ self[$0] }
+        }
+        
         // MARK: -
 
         public func insert(_ newMember: Element) {
-            
-
             if saved {
                 
             } else {
                 if !_Self.contains(newMember) {
+                    let indexes: IndexSet = IndexSet(integer: _Self.count)
+                    print(indexes)
+                    self.willChange(.insertion, valuesAt: indexes, forKey: keyPath!)
+//                    owner?.willChangeValue(forKey: keyPath!)
                     _Self.append(newMember)
+//                    owner?.didChangeValue(forKey: keyPath!)
+                    self.didChange(.insertion, valuesAt: indexes, forKey: keyPath!)
                 }
             }
+            
         }
 
         public func remove(_ member: Element) {
@@ -105,6 +114,16 @@ extension Salada {
             } else {
                 _Self = []
             }
+        }
+        
+        open override func setValue(_ value: Any?, forKeyPath keyPath: String) {
+            super.setValue(value, forKeyPath: keyPath)
+        }
+        
+        open override func mutableSetValue(forKeyPath keyPath: String) -> NSMutableSet {
+            let set = super.mutableSetValue(forKeyPath: keyPath)
+            print(set)
+            return set
         }
         
         // MARK: -
